3a09903e57fe537d98d15af02b569d6e5043f062
reymontero <78079378+reymontero@users.noreply.github.com>
2021-05-27 15:21:35 +0800

    Add files via upload

---
+++ OSOC86_140922/tools/CompareTestResults/CompareTestResults/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace CompareTestResults
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(449, 76);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Paths";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(416, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 20);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(94, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(416, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 20);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(94, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(316, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Test Ref. File:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Simulation path:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(206, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Compare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(11, 123);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(451, 313);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 445);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Open SOC 86 tests checker";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ OSOC86_140922/tools/CompareTestResults/CompareTestResults/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace CompareTestResults
+{
+    public partial class Form1 : Form
+    {
+        private List<List<string>> lstT = new List<List<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = @"..\..\..\..\..\project\cpu\simulation\modelsim\";
+            textBox2.Text = @"..\..\..\..\..\test\TestsResults.txt";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckResults();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            FBD.SelectedPath = Path.GetFullPath(textBox1.Text);
+            DialogResult DR = FBD.ShowDialog();
+            if (DR == DialogResult.OK)
+            {
+                string sTmp = FBD.SelectedPath;
+                if (sTmp != "")
+                    if (sTmp.Substring(sTmp.Length - 1, 1) != "\\")
+                        sTmp += "\\";
+                textBox1.Text = sTmp;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            OFD.FilterIndex = 1;
+            OFD.Multiselect = false;
+
+            OFD.InitialDirectory = Path.GetFullPath(Path.GetDirectoryName(textBox2.Text));
+            OFD.FileName = Path.GetFileName(textBox2.Text);
+
+            DialogResult DR = OFD.ShowDialog();
+            if (DR == DialogResult.OK)
+                textBox2.Text = OFD.FileName;
+        }
+
+        private void CheckResults()
+        {
+            string sFileT = textBox2.Text;
+            string sPathR = textBox1.Text;
+
+            LoadTestFile(sFileT);
+
+            List<string> Tests = new List<string>();
+            List<int> Inds = new List<int>();
+            for (int i = 1; i < 20; i++)
+                if (i != 9)
+                {
+                    string sTmp = Convert.ToString(i, 16);
+                    while (sTmp.Length < 2)
+                        sTmp = "0" + sTmp;
+
+                    Tests.Add("instlog" + sTmp + ".txt");
+                    Inds.Add(i);
+                }
+
+            for (int i = 0; i < Tests.Count; i++)
+            {
+                string sLog = "";
+                int cnt = Compare(sPathR + Tests[i], Inds[i], ref sLog);
+
+                int cntRef = lstT[Inds[i]].Count;
+                string sErr = "OK";
+                if ((cnt == 0) || (sLog != ""))
+                    sErr = "error";
+                textBox3.Text += Tests[i] + "\t" + "lines: sim=" + cnt.ToString() + ", ref=" + cntRef.ToString() + "\t"+ sErr + "\r\n";
+                textBox3.Text += sLog;// +"\r\n";
+            }
+
+            MessageBox.Show("Done");
+        }
+
+        private void LoadTestFile(string sFile)
+        {
+            lstT = new List<List<string>>();
+            int Ind = -1;
+
+            StreamReader SR = new StreamReader(sFile);
+            string sLine = null;
+            while ((sLine = SR.ReadLine()) != null)
+                if (sLine.Length > 2)
+                {
+                    if (sLine.Substring(2, 1) == "_")
+                    {
+                        Ind = Convert.ToInt32(sLine.Substring(0, 2));
+                        while (lstT.Count <= Ind)
+                            lstT.Add(new List<string>());
+                    }
+                    else if (sLine.Substring(0, 2) == "0x")
+                    {
+                        sLine = sLine.Substring(sLine.IndexOf(":"), sLine.Length - sLine.IndexOf(":"));
+                        sLine = sLine.Replace("0x", "");
+                        sLine = sLine.Replace("X", "0");
+                        lstT[Ind].Add(sLine);
+                    }
+                }
+            SR.Close();
+        }
+
+        private int Compare(string sFile, int Ind, ref string sLog)
+        {
+            List<string> lstR = new List<string>();
+
+            StreamReader SR = new StreamReader(sFile);
+            string sLine = null;
+            while ((sLine = SR.ReadLine()) != null)
+            {
+                if (sLine.IndexOf(":") != -1)
+                {
+                    sLine = sLine.Substring(sLine.IndexOf(":"), sLine.Length - sLine.IndexOf(":"));
+                    sLine = sLine.Replace("0x", "");
+                    sLine = sLine.Replace("X", "0");
+                    lstR.Add(sLine);
+                }
+            }
+            SR.Close();
+
+            sLog = "";
+            for (int i = 0; (i < lstR.Count) && (i < lstT[Ind].Count); i++)
+                if (lstR[i] != lstT[Ind][i])
+                    sLog += lstR[i] + "\r\n";
+
+            return lstR.Count;
+        }
+
+        
+
+    }
+}
